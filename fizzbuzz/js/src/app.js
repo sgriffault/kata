445f4886280f27a52106dffc13174ffed68fbcf1
sgriffault <simon.griffault@etu.u-bordeaux.fr>
2020-10-21 14:32:00 +0200

    implementation of fizzbuzz and main methods

--- fizzbuzz/js/src/app.js
+++ fizzbuzz/js/src/app.js
@@ -1,13 +1,21 @@
 // Impement fizzbuzz logic
 function fizzbuzz(number) {
-  return '0';
+  if(number % 15 == 0){
+    return "fizzbuzz";
+  }else if(number % 3 == 0){
+    return "fizz";
+  }else if(number % 5 == 0){
+    return "buzz";
+  }else {
+    return number;
+  }  
 }
 
 // Implement the wrapper so we can count from 0 to STOP.
-function main() {
-   for(let i=0; i<10;i++) {
-     console.log(fizzbuzz(i));
+function main(STOP) {
+   for(let i=0; i<=STOP; i++) {
+    console.log(fizzbuzz(i));
    }
 }
 
-main();
+main(20);
